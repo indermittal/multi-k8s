ade2203a73ed7d09f53005efb7c494c84e25b49c
Inder Mittal <inder.mittal@gmail.com>
2021-09-01 21:10:06 +0100

    Added scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t indermittal/multi-client:latest -t indermittal/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t indermittal/multi-server:latest -t indermittal/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t indermittal/multi-worker:latest -t indermittal/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push indermittal/multi-client:latest
+docker push indermittal/multi-server:latest
+docker push indermittal/multi-worker:latest
+
+docker push indermittal/multi-client:$SHA 
+docker push indermittal/multi-server:$SHA 
+docker push indermittal/multi-worker:$SHA 
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=indermittal/multi-server:$SHA
+kubectl set image deployments/client-deployment server=indermittal/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=indermittal/multi-worker:$SHA
